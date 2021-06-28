0b1b7f5f08aa4b226421bd4041d47b5797ccb451
anchal00 <anchal82199@gmail.com>
2021-06-28 22:59:12 +0530

    Added support for swagger

--- vehicles-api/src/main/java/com/udacity/vehicles/VehiclesApiApplication.java
+++ vehicles-api/src/main/java/com/udacity/vehicles/VehiclesApiApplication.java
@@ -12,22 +12,28 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.reactive.function.client.WebClient;
 
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 /**
- * Launches a Spring Boot application for the Vehicles API,
- * initializes the car manufacturers in the database,
- * and launches web clients to communicate with maps and pricing.
+ * Launches a Spring Boot application for the Vehicles API, initializes the car
+ * manufacturers in the database, and launches web clients to communicate with
+ * maps and pricing.
  */
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableEurekaServer
+@EnableSwagger2
 public class VehiclesApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(VehiclesApiApplication.class, args);
     }
 
+    
     /**
      * Initializes the car manufacturers available to the Vehicle API.
+     * 
      * @param repository where the manufacturer information persists.
      * @return the car manufacturers to add to the related repository
      */
@@ -49,20 +55,22 @@ public class VehiclesApiApplication {
 
     /**
      * Web Client for the maps (location) API
+     * 
      * @param endpoint where to communicate for the maps API
      * @return created maps endpoint
      */
-    @Bean(name="maps")
+    @Bean(name = "maps")
     public WebClient webClientMaps(@Value("${maps.endpoint}") String endpoint) {
         return WebClient.create(endpoint);
     }
 
     /**
      * Web Client for the pricing API
+     * 
      * @param endpoint where to communicate for the pricing API
      * @return created pricing endpoint
      */
-    @Bean(name="pricing")
+    @Bean(name = "pricing")
     public WebClient webClientPricing(@Value("${pricing.endpoint}") String endpoint) {
         return WebClient.create(endpoint);
     }
